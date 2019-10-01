47d4d1f286b8fd2b051347e936ec759d8954ece0
JoshSample <joshsample98@gmail.com>
2019-10-01 18:50:54 -0500

    Added some tokenizing

--- Lab1.py
+++ Lab1.py
@@ -3,7 +3,7 @@ Josh Sample
 CSCI3370
 Lab 1
 """
-import xml.etree.cElementTree as ET
+from xml.etree.ElementTree import Element, SubElement, ElementTree
 
 SYMBOLS = ['(', ')', '[', ']', '{', '}', ',', ';', '=', '.', '+', '-', '*', '/', '&', '|', '~', '<', '>']
 KEYWORDS = ['class', 'constructor', 'method', 'function', 'int', 'boolean', 'char', 'void', 'var',
@@ -14,26 +14,32 @@ def main():
     symbol = "symbol"
     reserved_word = "keyword"
     identifier = "identifier"
-    int_const = "integerConstant"
-    str_const = "stringConstant"
-    bool_const = "booleanConstant"
-    root = ET.Element("tokens")
+    root = Element("tokens")
 
     # This is the file reading process
     with open('Square.jack', 'r') as f:
         for line in f:
+            # Removes single line comments
             line = line.partition('//')[0]
+            # Removes multi line comments
             if line.__contains__('/**'):
                 while not line.__contains__('*/'):
                     line = f.__next__()
                 line = f.__next__()
-            print(line)
+            # Tokenize keywords
+            if any(x in KEYWORDS for x in line.split()):
+                keyw = SubElement(root, reserved_word)
+                keyw.text = line.split()[0]
+            # Tokenize symbols
+            for character in line:
+                if any(x in SYMBOLS for x in character):
+                    char = SubElement(root, symbol)
+                    char.text = character
+                    line = line.replace(character, '')
 
-    # ET.SubElement(root, "field1", name="blah").text = "some value1"
-    # ET.SubElement(root, "field2", name="asdfasd").text = "some value2"
     f.close()
-    # tree = ET.ElementTree(root)
-    # tree.write("filename.xml")
+    tree = ElementTree(root)
+    tree.write("filename.xml")
 
 
 if __name__ == "__main__":
