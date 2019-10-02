a39e0905cb6dc630013fc602728be3ce3c107527
JoshSample <joshsample98@gmail.com>
2019-10-02 18:21:01 -0500

    Commit finished with all xml

--- Lab1.py
+++ Lab1.py
@@ -18,6 +18,7 @@ def main():
     root = Element("tokens")
 
     # This is the file reading process
+    # writing Square xml
     with open('Square.jack', 'r') as f:
         for line in f:
             # Removes single line comments
@@ -49,13 +50,125 @@ def main():
                     iden = SubElement(root, identifier)
                     iden.text = word
                     line = line.replace(line, '')
-            print(line.split())
     # Close file
     f.close()
     # Write xml file
     tree = ElementTree(root)
-    tree.write("filename.xml")
+    tree.write("SquareT.xml")
 
+    # SquareGame xml
+    with open('SquareGame.jack', 'r') as f:
+        for line in f:
+            # Removes single line comments
+            line = line.partition('//')[0]
+            # Removes multi line comments
+            if line.__contains__('/**'):
+                while not line.__contains__('*/'):
+                    line = f.__next__()
+                line = f.__next__()
+            # Tokenize keywords
+            count = 0
+            for word in line:
+                if any(word in KEYWORDS for word in line.split()):
+                    keyw = SubElement(root, reserved_word)
+                    keyw.text = line.split()[count]
+                    line = line.replace(line.split()[count], '')
+                    count += count
+                for character in line:
+                    if any(x in SYMBOLS for x in character):
+                        char = SubElement(root, symbol)
+                        char.text = character
+                        line = line.replace(character, '')
+                    if any(x in DIGITS for x in character):
+                        line = line.replace(character, '')
+            for word in line.split():
+                if not word:
+                    continue
+                else:
+                    iden = SubElement(root, identifier)
+                    iden.text = word
+                    line = line.replace(line, '')
+        # Close file
+        f.close()
+        # Write xml file
+        tree = ElementTree(root)
+        tree.write("SquareGameT.xml")
+
+    # MainSquare xml
+    with open('MainSquare.jack', 'r') as f:
+        for line in f:
+            # Removes single line comments
+            line = line.partition('//')[0]
+            # Removes multi line comments
+            if line.__contains__('/**'):
+                while not line.__contains__('*/'):
+                    line = f.__next__()
+                line = f.__next__()
+            # Tokenize keywords
+            count = 0
+            for word in line:
+                if any(word in KEYWORDS for word in line.split()):
+                    keyw = SubElement(root, reserved_word)
+                    keyw.text = line.split()[count]
+                    line = line.replace(line.split()[count], '')
+                    count += count
+                for character in line:
+                    if any(x in SYMBOLS for x in character):
+                        char = SubElement(root, symbol)
+                        char.text = character
+                        line = line.replace(character, '')
+                    if any(x in DIGITS for x in character):
+                        line = line.replace(character, '')
+            for word in line.split():
+                if not word:
+                    continue
+                else:
+                    iden = SubElement(root, identifier)
+                    iden.text = word
+                    line = line.replace(line, '')
+        # Close file
+        f.close()
+        # Write xml file
+        tree = ElementTree(root)
+        tree.write("MainSquareT.xml")
+
+        # Main xml
+        with open('Main.jack', 'r') as f:
+            for line in f:
+                # Removes single line comments
+                line = line.partition('//')[0]
+                # Removes multi line comments
+                if line.__contains__('/**'):
+                    while not line.__contains__('*/'):
+                        line = f.__next__()
+                    line = f.__next__()
+                # Tokenize keywords
+                count = 0
+                for word in line:
+                    if any(word in KEYWORDS for word in line.split()):
+                        keyw = SubElement(root, reserved_word)
+                        keyw.text = line.split()[count]
+                        line = line.replace(line.split()[count], '')
+                        count += count
+                    for character in line:
+                        if any(x in SYMBOLS for x in character):
+                            char = SubElement(root, symbol)
+                            char.text = character
+                            line = line.replace(character, '')
+                        if any(x in DIGITS for x in character):
+                            line = line.replace(character, '')
+                for word in line.split():
+                    if not word:
+                        continue
+                    else:
+                        iden = SubElement(root, identifier)
+                        iden.text = word
+                        line = line.replace(line, '')
+            # Close file
+            f.close()
+            # Write xml file
+            tree = ElementTree(root)
+            tree.write("Main.xml")
 
 if __name__ == "__main__":
     main()
